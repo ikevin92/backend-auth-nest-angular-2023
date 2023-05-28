299519dcba0b2318d23b774ef339eac0d7241dfa
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-05-28 17:06:41 -0500

    conectar con nest mongo db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017'), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
