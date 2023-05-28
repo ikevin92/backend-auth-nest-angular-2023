dfec947a65b9025c6fd73b328dd78d22dd374907
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-05-28 17:46:22 -0500

    crear un registro en la bd

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class AuthService {
-  create(createUserDto: CreateUserDto) {
-    console.log({ createUserDto });
-    return 'This action adds a new auth';
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    try {
+      const newUser = new this.userModel(createUserDto);
+
+      // 1. Encriptar contraseña
+      // 2. Guardar Usuario
+      // 3. Generar el JWT
+      return await newUser.save();
+    } catch (error) {
+      console.log({ error });
+      if (error.code === 11000) {
+        throw new BadRequestException(`${createUserDto.email} already exist`);
+      }
+      throw new InternalServerErrorException('Something terrible happened');
+    }
   }
 
   findAll() {
